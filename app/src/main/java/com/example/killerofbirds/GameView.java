11536b8d6f3365cb78eefb239f6a8543971d526d
Eugene Larin <lar170400@yahoo.com>
2020-01-14 16:23:12 +0300

    App

--- app/src/main/java/com/example/killerofbirds/GameView.java
+++ app/src/main/java/com/example/killerofbirds/GameView.java
@@ -3,6 +3,7 @@ package com.example.killerofbirds;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
 
 public class GameView extends SurfaceView implements Runnable{
@@ -10,19 +11,23 @@ public class GameView extends SurfaceView implements Runnable{
     private boolean isPlaying;
     private int screenX;
     private int screenY;
-    private float screeenRatioX, screenRatioY;
+    public static float screenRatioX, screenRatioY;
     private Paint paint;
+    private Flight flight;
     private Background background1, background2;
 
     public GameView(Context context, int screenX, int screenY) {
         super(context);
         this.screenX = screenX;
         this.screenY = screenY;
-        screeenRatioX = 1920f / screenX;
+        screenRatioX = 1920f / screenX;
         screenRatioY = 1080f / screenY;
 
         background1 = new Background(screenX, screenY, getResources());
         background2 = new Background(screenX, screenY, getResources());
+
+        flight = new Flight(screenY, getResources());
+
         background2.x = screenX;
 
         paint = new Paint();
@@ -37,8 +42,8 @@ public class GameView extends SurfaceView implements Runnable{
         }
     }
     private void update () {
-        background1.x -= 10 * screeenRatioX;
-        background2.x -= 10 * screeenRatioX;
+        background1.x -= 10 * screenRatioX;
+        background2.x -= 10 * screenRatioX;
 
         if (background1.x + background1.background.getWidth() < 0) {
             background1.x = screenX;
@@ -46,12 +51,25 @@ public class GameView extends SurfaceView implements Runnable{
         if (background2.x + background2.background.getWidth() < 0) {
             background2.x = screenX;
         }
+        if (flight.isGoingUp)
+            flight.y -= 30 * screenRatioY;
+        else
+            flight.y += 30 * screenRatioY;
+
+        if (flight.y < 0)
+            flight.y = 0;
+
+        if (flight.y >= screenY - flight.height)
+            flight.y = screenY - flight.height;
     }
     private void draw () {
         if (getHolder().getSurface().isValid()) {
             Canvas canvas = getHolder().lockCanvas();
             canvas.drawBitmap(background1.background, background1.x, background1.y, paint);
             canvas.drawBitmap(background2.background, background2.x, background2.y, paint);
+
+            canvas.drawBitmap(flight.getFlight(), flight.x, flight.y, paint);
+
             getHolder().unlockCanvasAndPost(canvas);
 
         }
@@ -75,5 +93,21 @@ public class GameView extends SurfaceView implements Runnable{
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case  MotionEvent.ACTION_DOWN:
+                if (event.getX() < screenX / 2) {
+                    flight.isGoingUp = true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                flight.isGoingUp = false;
+                break;
+        }
+        return true;
     }
 }
